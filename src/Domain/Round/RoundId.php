fb0b46db77ca713d56ca220a0f5a06131d3d3d4a
Patryk Woziński <patryk.wozinski@gmail.com>
2018-12-03 14:15:33 +0100

    Setup

---
+++ src/Domain/Round/RoundId.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freeq
+ * Date: 03/12/2018
+ * Time: 14:06
+ */
+declare(strict_types=1);
+
+namespace CompanyRoulette\Domain\Round;
+
+
+final class RoundId
+{
+}
